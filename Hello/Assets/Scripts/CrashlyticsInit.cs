e1acc44db26b8d17520e036167b7e7f3e8fe4289
lanyantao <lanyantaoly@163.com>
2022-06-02 10:15:12 +0800

    test project

---
+++ Hello/Assets/Scripts/CrashlyticsInit.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Import Firebase
+//using Firebase;
+using UnityEngine.UI;
+
+public class CrashlyticsInit : MonoBehaviour {
+    public Text label;
+    private string msg;
+    public Button init;
+    public Button crash;
+    private bool isSafe;
+
+    private void Awake()
+    {
+        init.onClick.AddListener(() => Init());
+        crash.onClick.AddListener(() => CrashTest());
+    }
+
+    // Use this for initialization
+    void Start () {
+        // Initialize Firebase
+        //Init();
+    }
+
+    private void Update()
+    {
+        this.label.text = isSafe + "   " + msg;
+    }
+
+    private void Init()
+    {
+        Firebase.FirebaseApp.CheckAndFixDependenciesAsync().ContinueWith(task => {
+            var dependencyStatus = task.Result;
+            msg = "11111111";
+            if (dependencyStatus == Firebase.DependencyStatus.Available)
+            {
+                // Create and hold a reference to your FirebaseApp,
+                // where app is a Firebase.FirebaseApp property of your application class.
+                // Crashlytics will use the DefaultInstance, as well;
+                // this ensures that Crashlytics is initialized.
+                Firebase.FirebaseApp.LogLevel = Firebase.LogLevel.Debug;
+                Firebase.FirebaseApp app = Firebase.FirebaseApp.DefaultInstance;
+                // Set a flag here for indicating that your project is ready to use Firebase.
+
+                msg = "firebase Available";
+                isSafe = true;
+            }
+            else
+            {
+                msg = dependencyStatus.ToString();
+                isSafe = false;
+                // Firebase Unity SDK is not safe to use here.
+            }
+        });
+    }
+
+    private void CrashTest()
+    {
+        throw new System.Exception("test exception please ignore");
+    }
+}
---
+++ Hello/Assets/Scripts/CrashlyticsTester.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CrashlyticsTester : MonoBehaviour
+{
+
+    public Text label;
+    int updatesBeforeException;
+
+    // Use this for initialization
+    void Start () {
+      updatesBeforeException = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Call the exception-throwing method here so that it's run
+        // every frame update
+        throwExceptionEvery60Updates();
+    }
+
+    private int times = 1;
+    // A method that tests your Crashlytics implementation by throwing an
+    // exception every 60 frame updates. You should see non-fatal errors in the
+    // Firebase console a few minutes after running your app with this method.
+    void throwExceptionEvery60Updates()
+    {
+        if (updatesBeforeException > 0)
+        {
+            updatesBeforeException--;
+        }
+        else
+        {
+            // Set the counter to 60 updates
+            updatesBeforeException = 60;
+
+            ++times;
+            label.text = times.ToString();
+            // Throw an exception to test your Crashlytics implementation
+            
+            GameObject err = null;
+            err.SetActive(false);
+            //throw new System.Exception("test exception please ignore");
+        }
+    }
+}
